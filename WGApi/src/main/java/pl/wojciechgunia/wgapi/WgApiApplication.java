60905776f9430e7f375ace0093031e501c4dcf0b
Wojciech Gunia <wojciech.gunia@outlook.com>
2025-02-20 13:56:24 +0100

    Project initialization

---
+++ WGApi/src/main/java/pl/wojciechgunia/wgapi/WgApiApplication.java
@@ -0,0 +1,13 @@
+package pl.wojciechgunia.wgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WgApiApplication.class, args);
+    }
+
+}
---
+++ WGApi/src/test/java/pl/wojciechgunia/wgapi/WgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciechgunia.wgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
